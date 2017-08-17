3d056fc47e295a481287911f995415d9028b1a66
Anthony Frugier <fru.anthony@gmail.com>
2017-08-17 10:36:04 +0200

    Définition d’une fonction

---
+++ 01-rappels-es5/01.function.js
@@ -0,0 +1,12 @@
+console.log("01 - Fonctions");
+
+var nombre1 = 10;
+var nombre2 = 20;
+
+function additionner(nb1, nb2){
+    return nb1+nb2;
+}
+
+var resultat1 = additionner(nombre1,nombre2);
+
+console.log("resultat ==",resultat1);
