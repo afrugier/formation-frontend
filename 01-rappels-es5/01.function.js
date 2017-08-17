f73cc55199a6a28b81e1a79c2dd25b9a6aed9e3e
Anthony Frugier <fru.anthony@gmail.com>
2017-08-17 11:08:04 +0200

    Fonction comme élément du 1er ordre

--- 01-rappels-es5/01.function.js
+++ 01-rappels-es5/01.function.js
@@ -5,15 +5,23 @@ var nombre2 = 20;
 function additionner(nb1, nb2){
     return nb1+nb2;
 }
-var resultat1 = additionner(nombre1,nombre2);
-console.log("resultat1 ==",resultat1);
+var resultat1 = additionner(nombre1, nombre2);
+console.log("resultat1 ==", resultat1);
 
 
 var somme = additionner;
-var resultat2 = somme(nombre1,nombre2);
+var resultat2 = somme(nombre1, nombre2);
 console.log("resultat2 ==",resultat2);
-var multiplication = function(nb1,nb2){
+var multiplication = function(nb1, nb2){
     return nb1*nb2;
 };
-var resultat3 = multiplication(nombre1,nombre2);
+var resultat3 = multiplication(nombre1, nombre2);
 console.log("resultat3 ==",resultat3);
+
+
+var afficherOperation = function(nomOperation, operation, nb1, nb2){
+    console.log(nomOperation,"[ nb1 ==",nb1,"nb2 ==",nb2,"] ==",operation(nb1,nb2));
+};
+resultat4 = afficherOperation("Somme", somme, 20, 40);
+resultat4 = afficherOperation("Multiplication", multiplication,10,20);
+resultat4 = afficherOperation("Soustraction", function(nb1,nb2){return nb1-nb2;},15,5);
