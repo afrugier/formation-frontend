57662bcbe524ebca91a5d1afe438f0ea6fd00235
Anthony Frugier <fru.anthony@gmail.com>
2017-08-18 14:26:52 +0200

    Premiers pas avec Webpack

---
+++ 03-webpack/premiers-pas-webpack/webpack.config.js
@@ -0,0 +1,11 @@
+var path = require('path');
+
+module.exports = {
+    // définition des points d'entrée
+    // il est possible de définir plusieurs points d'entrée
+    entry: './app/index.js',
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "bundle.js"
+    }
+}
